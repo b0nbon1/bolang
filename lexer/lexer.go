eeff493dd4011859d950b7c4091676a7c1e3093b
Bonvic <bundi.bonvic@gmail.com>
2025-07-16 15:25:21 +0300

    define lexer and tokens

---
+++ lexer/lexer.go
@@ -0,0 +1,77 @@
+package lexer
+
+import "bonlang/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar() // read the characters from the input
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // 0 is the zero value for byte, representing EOF or nothing to read
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+		case '=':
+			tok = newToken(token.ASSIGN, l.ch)
+		case ';':
+			tok = newToken(token.SEMICOLON, l.ch)
+		case '(':
+			tok = newToken(token.LPAREN, l.ch)
+		case ')':
+			tok = newToken(token.RPAREN, l.ch)
+		case ',':
+			tok = newToken(token.COMMA, l.ch)
+		case '+':
+			tok = newToken(token.PLUS, l.ch)
+		case '{':
+			tok = newToken(token.LBRACE, l.ch)
+		case '}':
+			tok = newToken(token.RBRACE, l.ch)
+		case 0:
+			tok.Literal = ""
+			tok.Type = token.EOF
+		default:
+			if isLetter(l.ch) {
+				tok.Literal = l.readIdentifier()
+				return tok
+			}
+			tok = newToken(token.ILLEGAL, l.ch)
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
